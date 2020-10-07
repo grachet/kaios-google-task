9e857c7de6922e1bb4a93cbb76a5fc97de52194c
Guillaume Rachet <36331252+grachet@users.noreply.github.com>
2020-10-08 00:28:25 +0200

    usable app

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,76 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useGoogleLogin } from 'react-google-login';
 import './App.scss';
 import CheckboxListItem from './components/CheckboxListItem/CheckboxListItem';
 import { SoftKeyProvider } from './components/SoftKey/SoftKeyProvider';
-import { ToastContextProvider } from './contexts/toastContext';
+import ToastContext, { ToastContextProvider } from './contexts/toastContext';
+import taskService from './services/taskService';
+import config from './services/config.json';
 import ListView from './views/ListView/ListView';
 import TabView from './views/TabView/TabView';
+import { isLocalhost } from "./services/helperFunctions"
+import ProgressBar from './components/ProgressBar/ProgressBar';
 
 function App() {
 
+  const [isSignedIn, setSignedIn] = useState(false);
+  const [taskList, setTaskList] = useState([]);
+
+  const { signIn, loaded } = useGoogleLogin({
+    clientId: config.client_id,
+    onSuccess: async (rep) => {
+      console.log(rep)
+      await taskService.init()
+      setTaskList(await taskService.getAllTaskList())
+      setSignedIn(true)
+    },
+    isSignedIn: true,
+    // cookiePolicy:'none',
+    redirectUri: !!isLocalhost ? "http://localhost:3000" : "https://grachet.github.io/kaios-google-task",
+    onFailure: (error) => {
+      console.error(error)
+    },
+    uxMode: "redirect",
+    scope: 'https://www.googleapis.com/auth/tasks',
+  })
+
+  useEffect(() => {
+    loaded && signIn()
+  }, []);
+
+  console.log(taskList)
+
   const App = () => {
-    // const { showToast } = useContext(ToastContext);
+    const { showToast } = useContext(ToastContext);
     return (
       <div className="App">
-        <SoftKeyProvider
-
-        >
+        {!taskList.length && <ProgressBar
+          header={'Loading...'}
+          percentage={30}
+          type={"buffer"}
+        />}
+        <SoftKeyProvider>
           <div className="content">
             <TabView
-              tabLabels={[
-                'Task list 1',
-                'Task list 2'
-              ]}
+              tabLabels={taskList.map(list => list.title)}
             >
-              <ListView>
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
+              {taskList.map((list, i) => <ListView key={i}>
+                {
+                  list.tasks.map((task, ti) => <CheckboxListItem
+                    key={ti}
+                    primary={task.title}
+                    initCheckboxVal={task.status === "completed"}
+                    checkboxSide="left"
+                    onInputChange={(isChecked) => taskService.updateTask({
+                      ...task,
+                      taskListId: list.id,
+                      taskId: task.id,
+                      status: isChecked ? "completed" : "needsAction"
+                    }).catch(err => showToast("Fail to update GTask", 5000))}
+                  />)
+                }
               </ListView>
-              <ListView>
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-                <CheckboxListItem
-                  primary="Hello primary text"
-                  secondary="seconday text"
-                  initCheckboxVal={false}
-                  checkboxSide="left"
-                />
-              </ListView>
+              )}
             </TabView>
           </div>
         </SoftKeyProvider>
