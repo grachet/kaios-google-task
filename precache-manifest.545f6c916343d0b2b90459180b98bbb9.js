a4f2f34395358262f8be45c9b41c203e9cdd26c9
Guillaume Rachet <36331252+grachet@users.noreply.github.com>
2020-10-07 18:54:36 +0200

    Updates

---
+++ precache-manifest.545f6c916343d0b2b90459180b98bbb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50a846eeccb043fa268e97f2c43c7be",
+    "url": "/kaios-google-task/index.html"
+  },
+  {
+    "revision": "a639ae7ea3e505fe227b",
+    "url": "/kaios-google-task/static/css/main.b243b623.chunk.css"
+  },
+  {
+    "revision": "5cb6f9a2851f991bf5a2",
+    "url": "/kaios-google-task/static/js/2.f942d3fb.chunk.js"
+  },
+  {
+    "revision": "a639ae7ea3e505fe227b",
+    "url": "/kaios-google-task/static/js/main.b56c7e31.chunk.js"
+  },
+  {
+    "revision": "100d7fd0faed5ee7e314",
+    "url": "/kaios-google-task/static/js/runtime~main.e6931bc3.js"
+  }
+]);
