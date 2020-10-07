52a72946e150c3ae1503ee968e8f9ff8a97dd4ba
Guillaume Rachet <36331252+grachet@users.noreply.github.com>
2020-10-07 22:10:22 +0200

    clean with get tasks

--- src/App.js
+++ src/App.js
@@ -2,55 +2,43 @@ import React, { useEffect, useState } from "react";
 import config from "./config.json"
 import { Header, Input, Softkey, ToDos } from "./components";
 import { useNavigation } from "./hooks";
-// import taskService from "./service/taskService";
+import taskService from "./service/taskService";
 import { useGoogleLogin } from 'react-google-login'
 import { isLocalhost } from "./service/helperFunctions";
 
 export default function App() {
 
   const [isSignedIn, setSignedIn] = useState(false);
+  const [taskList, setTaskList] = useState(false);
 
   const { signIn, loaded } = useGoogleLogin({
     clientId: config.web.client_id,
-    onSuccess: (rep) => {
+    onSuccess: async (rep) => {
       console.log(rep)
+      await taskService.init()
+      let listTaskLists = await taskService.listTaskLists()
+      setTaskList(listTaskLists)
       setSignedIn(true)
     },
-    // onAutoLoadFinished,
-    // clientId,
-    // cookiePolicy: "none",
-    // loginHint,
-    // hostedDomain,
-    // autoLoad,
     isSignedIn: true,
-    // fetchBasicProfile,
+    // cookiePolicy:'none',
     redirectUri: !!isLocalhost ? "http://localhost:3000" : "https://grachet.github.io/kaios-google-task",
-    // discoveryDocs,
     onFailure: (error) => {
       console.error(error)
     },
     uxMode: "redirect",
     scope: 'https://www.googleapis.com/auth/tasks',
-    // accessType,
-    // responseType,
-    // jsSrc,
-    onRequest: (rep) => {
-      console.log(rep)
-    },
-    // prompt
   })
 
+  console.log(taskList)
+
   const sign = () => {
-    console.log(loaded, signIn)
     loaded && signIn()
-    // taskService.init()
   }
 
   return (
     <>
-      <Header title="Google task 2" />
-
-      <button onClick={() => sign()}>Sign</button>
+      {!!isLocalhost && <button onClick={() => sign()}>Sign</button>}
 
       <Softkey
         left={isSignedIn ? null : "Sign In"}
